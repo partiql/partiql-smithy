4410ee442b36036c6eb28c60b76f8a8f7580409f
R. C. Howell <howero@amazon.com>
2024-11-18 14:25:06 -0800

    Backup whatever I was doing on friday

--- anodizer/anodizer-core/src/main/kotlin/io/github/amzn/anodizer/AnodizerModel.kt
+++ anodizer/anodizer-core/src/main/kotlin/io/github/amzn/anodizer/AnodizerModel.kt
@@ -3,7 +3,7 @@ package io.github.amzn.anodizer
 import io.github.amzn.anodizer.core.Definition
 
 /**
- * Top-level model of an anodizer type domain.
+ * Top-level model of an anodizer domain (collection of types).
  *
  * @property domain         The top-level namespace name.
  * @property definitions    The top-level definitions.
@@ -13,23 +13,14 @@ public class AnodizerModel(
     @JvmField public val definitions: List<Definition>,
 ) {
 
-    override fun toString(): String = buildString {
-        appendLine("Domain: $domain")
-        appendLine("Types:")
-        for (d in definitions) {
-            append("‣ ")
-            append(d)
-            appendLine()
-        }
-    }
-
-    /**‣
-     * Remove all aliases and replace references to the alias with its base.
+    /**
+     * Return a debug string.
      */
-    public fun normalize(): AnodizerModel {
-        // TODO
-        //  1. Remove all aliases.
-        //  2. Move all inlines to top-level of current namespace.
-        return this
+    override fun toString(): String = buildString {
+        appendLine(domain)
+        appendLine("-----------------------------")
+        for (d in definitions) {
+            appendLine(d)
+        }
     }
 }
